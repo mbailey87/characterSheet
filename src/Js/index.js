3fde9db739736fecccd6b104804bb7e2aa0102de
Matthew Bailey <matthewbailey.webdev@gmail.com>
2024-06-17 12:52:54 -0600

    fixed classskillbonus

--- src/Js/index.js
+++ src/Js/index.js
@@ -3,41 +3,41 @@ const stats = [
 ];
 
 const skills = [
-    { skill: 'Acrobatics', stat: 'Dex' },
-    { skill: 'Appraise', stat: 'Int' },
-    { skill: 'Bluff', stat: 'Cha' },
-    { skill: 'Climb', stat: 'Str' },
-    { skill: 'Craft', stat: 'Int' },
-    { skill: 'Diplomacy', stat: 'Cha' },
-    { skill: 'Disable Device', stat: 'Dex' },
-    { skill: 'Disguise', stat: 'Cha' },
-    { skill: 'Escape Artist', stat: 'Dex' },
-    { skill: 'Fly', stat: 'Dex' },
-    { skill: 'Handle Animal', stat: 'Cha' },
-    { skill: 'Heal', stat: 'Wis' },
-    { skill: 'Intimidate', stat: 'Cha' },
-    { skill: 'Knowledge (arcana)', stat: 'Int' },
-    { skill: 'Knowledge (dungeoneering)', stat: 'Int' },
-    { skill: 'Knowledge (engineering)', stat: 'Int' },
-    { skill: 'Knowledge (geography)', stat: 'Int' },
-    { skill: 'Knowledge (history)', stat: 'Int' },
-    { skill: 'Knowledge (local)', stat: 'Int' },
-    { skill: 'Knowledge (nature)', stat: 'Int' },
-    { skill: 'Knowledge (nobility)', stat: 'Int' },
-    { skill: 'Knowledge (planes)', stat: 'Int' },
-    { skill: 'Knowledge (religion)', stat: 'Int' },
-    { skill: 'Linguistics', stat: 'Int' },
-    { skill: 'Perception', stat: 'Wis' },
-    { skill: 'Perform', stat: 'Cha' },
-    { skill: 'Profession', stat: 'Wis' },
-    { skill: 'Ride', stat: 'Dex' },
-    { skill: 'Sense Motive', stat: 'Wis' },
-    { skill: 'Sleight of Hand', stat: 'Dex' },
-    { skill: 'Spellcraft', stat: 'Int' },
-    { skill: 'Stealth', stat: 'Dex' },
-    { skill: 'Survival', stat: 'Wis' },
-    { skill: 'Swim', stat: 'Str' },
-    { skill: 'Use Magic Device', stat: 'Cha' },
+    { skill: 'Acrobatics', stat: 'Dexterity', abriv: 'Dex' },
+    { skill: 'Appraise', stat: 'Intelligence', abriv: 'Int' },
+    { skill: 'Bluff', stat: 'Charisma', abriv: 'Cha'},
+    { skill: 'Climb', stat: 'Strength', abriv: 'Str' },
+    { skill: 'Craft', stat: 'Intelligence', abriv: 'Int'},
+    { skill: 'Diplomacy', stat: 'Charisma', abriv: 'Cha' },
+    { skill: 'Disable Device', stat: 'Dexterity', abriv: 'Dex'},
+    { skill: 'Disguise', stat: 'Charisma', abriv: 'Cha' },
+    { skill: 'Escape Artist', stat: 'Dexterity', abriv: 'Dex' },
+    { skill: 'Fly', stat: 'Dexterity', abriv: 'Dex' },
+    { skill: 'Handle Animal', stat: 'Charisma', abriv: 'Cha'},
+    { skill: 'Heal', stat: 'Wisdom', abriv: 'Wis' },
+    { skill: 'Intimidate', stat: 'Charisma', abriv: 'Cha'},
+    { skill: 'Knowledge (arcana)', stat: 'Intelligence', abriv: 'Int' },
+    { skill: 'Knowledge (dungeoneering)', stat: 'Intelligence', abriv: 'Int' },
+    { skill: 'Knowledge (engineering)', stat: 'Intelligence', abriv: 'Int' },
+    { skill: 'Knowledge (geography)', stat: 'Intelligence', abriv: 'Int' },
+    { skill: 'Knowledge (history)', stat: 'Intelligence', abriv: 'Int' },
+    { skill: 'Knowledge (local)', stat: 'Intelligence', abriv: 'Int' },
+    { skill: 'Knowledge (nature)', stat: 'Intelligence', abriv: 'Int' },
+    { skill: 'Knowledge (nobility)', stat: 'Intelligence', abriv: 'Int'},
+    { skill: 'Knowledge (planes)', stat: 'Intelligence', abriv: 'Int'},
+    { skill: 'Knowledge (religion)', stat: 'Intelligence', abriv: 'Int' },
+    { skill: 'Linguistics', stat: 'Intelligence', abriv: 'Int' },
+    { skill: 'Perception', stat: 'Wisdom', abriv: 'Wis'},
+    { skill: 'Perform', stat: 'Charisma', abriv: 'Cha'},
+    { skill: 'Profession', stat: 'Wisdom', abriv: 'Wis'},
+    { skill: 'Ride', stat: 'Dexterity', abriv: 'Dex'},
+    { skill: 'Sense Motive', stat: 'Wisdom', abriv: 'Wis'},
+    { skill: 'Sleight of Hand', stat: 'Dexterity', abriv: 'Dex'},
+    { skill: 'Spellcraft', stat: 'Intelligence', abriv: 'Int'},
+    { skill: 'Stealth', stat: 'Dexterity', abriv: 'Dex'},
+    { skill: 'Survival', stat: 'Wisdom', abriv: 'Wis'},
+    { skill: 'Swim', stat: 'Strength', abriv: 'Str'},
+    { skill: 'Use Magic Device', stat: 'Charisma', abriv: 'Cha'},
 ];
 
 function generateStatRows() {
@@ -64,7 +64,7 @@ function generateSkillRows() {
         row.classList.add('text-center');
         row.innerHTML = `
             <td class="p-2 border border-gray-300">${skill.skill}</td>
-            <td class="p-2 border border-gray-300">${skill.stat}</td>
+            <td class="p-2 border border-gray-300">${skill.abriv}</td>
             <td class="p-2 border border-gray-300"><input class="w-10 border-gray-300" type="number" id="${skill.skill}Tot" value="0" readonly></td>
             <td class="p-2 border border-gray-300"><input class="w-10 border-gray-300" type="number" id="${skill.skill}Mod" value="0" readonly></td>
             <td class="p-2 border border-gray-300"><input class="w-10 border-gray-300" type="number" id="${skill.skill}Ranks" value="0"></td>
@@ -100,7 +100,7 @@ function calculateSkills() {
         const skillMisc = parseInt(document.getElementById(`${skill.skill}Misc`).value) || 0;
         const classSkill = document.getElementById(`${skill.skill}Class`);
         const statModifier = parseInt(modifiers[skill.stat]) || 0;
-        const classSkillBonus = classSkill.checked ? 3 : 0;
+        const classSkillBonus = classSkill.checked && skillRanks > 0 ? 3 : 0;
         skillMod.value = statModifier;
         skillTot.value = skillRanks + statModifier + skillMisc + classSkillBonus;
     });
